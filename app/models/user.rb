8ad154d71af3698642d7befe4d3e7141dc53f643
Tyler Krys <tylerkrys@gmail.com>
2017-08-07 21:04:43 +0000

    Adds validations for first_name, last_name, email, password, and password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+  validates :password, length: { in: 4..72 }
+  validates :password_confirmation, presence: true
+
 end
