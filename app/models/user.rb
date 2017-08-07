8478867a6455a314c6885545a5545d2ac44a6e76
Tyler Krys <tylerkrys@gmail.com>
2017-08-07 22:18:49 +0000

    Adds authenticate_with_credentials class method to allow testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,12 @@ class User < ActiveRecord::Base
   validates :password, length: { in: 4..72 }
   validates :password_confirmation, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+    if @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
 end
